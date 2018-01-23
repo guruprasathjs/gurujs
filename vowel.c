3b8a3930e7e55b52ad3cd8fd9ead46d234fb991f
guruprasathjs <35652213+guruprasathjs@users.noreply.github.com>
2018-01-23 16:23:53 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
  char g;
- clrscr
  scanf("%c",&g);
  if(g=='a'||g=='e'||g=='i'||g=='o'||g=='u')
- printf("vowel"):
+ printf("vowel");
  else
  printf("consonant");
- getch();
 }

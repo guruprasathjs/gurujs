b6e7fef604d24d7b0e3b835fe6b9e11944d68cf7
guruprasathjs <35652213+guruprasathjs@users.noreply.github.com>
2018-02-08 16:55:33 +0530

    Create armstrong bw 2 int.c

---
+++ armstrong bw 2 int.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int number,originalnumber,result=0;
+printf("enter the amstrong number");
+scanf("%d",&a);
+orignal number= number;
+while(original number !=0)
+{
+result +=orignal number;
+original number =10;
+}
+return 0;
+}
